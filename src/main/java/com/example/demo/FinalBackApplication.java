78599f4524e913e2547075914e676923922b37f9
jurguentaker13 <jurguentaker13@gmail.com>
2024-05-10 17:51:26 -0600

    FINAL-BACKEND

---
+++ src/main/java/com/example/demo/FinalBackApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class FinalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBackApplication.class, args);
+	}
+ @Bean
+ public WebMvcConfigurer corsConfigurer2(){
+     return new WebMvcConfigurer(){
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowCredentials(true)
+                .allowedHeaders("Origin", "Authorization", "Content-Type")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+   };
+ }
+}
